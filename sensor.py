794609b16e4786b89d312ddbe7cd7afeb088c295
MadsBT <madsfromdenmark@gmail.com>
2022-11-28 14:33:38 +0100

    Python SenseHat

--- sensor.py
+++ sensor.py
@@ -1,50 +1,93 @@
 from sense_hat import SenseHat
 from random import randint
-from socket import *
+# from socket import *
 from datetime import datetime
+import math
 import os
 
 sense = SenseHat()
-s = socket(AF_INET, SOCK_DGRAM)
-s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-BROADCAST_PORT = 7001;
+# s = socket(AF_INET, SOCK_DGRAM)
+# s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+# BROADCAST_PORT = 7001;
 
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
+colors = {"empty": (0, 0, 0), "red": (255, 0, 0), "green": (0, 255, 0), "blue": (0, 0, 255)}
 
-last_change = datetime.utcnow()
-#Makes sure that a specified amount of time, has passed, since the last call of the function.
-def anti_spam(event):
+
+# Function that gets the MAC-address of the Raspberry IP, and returns it.
+def get_sensor_mac_address():
+    # Placeholder MAC-Address
+    return "00:00:5e:00:53:af"
+
+
+# Function that gets called, once the joystick on the SenseHat is fired.
+def trigger_open(event):
+    # Only fire if the event is pressed.
     if event.action == "pressed":
-        global last_change
-        if (datetime.utcnow() - last_change).seconds >= 10:
-            stick_direction_change(event)
-            last_change = datetime.utcnow();
+        handle_input(get_id_from_humid())
 
 
-#Represents an event, that should be fired once a "user" tries to unlock the door with a key card.
-#Currently using the JoyStick of the SenseHat PI.
-card_ids = { "up": 1, "down": 2, "left": 3, "right": 4 }
-def stick_direction_change(event):
-    id_key_card = card_ids[event.direction]
-    id_door = 1;
-    msg = "door opened" if event.action == "pressed" else "door closed"
-    broadcast_result(id_key_card, id_door, msg)
-    show_result(id_key_card, id_door, msg)
+# Function that should be called, to generate a test id.
+def get_id_from_humid():
+    return math.floor(sense.get_humidity() / 10)
 
 
-def broadcast_result(id_key_card, id_door, msg):
-    data = str(id_key_card) + "," + str(id_door) + "," + msg
-    s.sendto(data.encode(), ("255.255.255.255", BROADCAST_PORT))
-    print("Broad casted: " + data)
+# Function that handles the input, and reflecting that in the SenseHat display.
+# where colors define what happened, if anything.
+# Function can also only ever execute, if it's been more that X-seconds, since last executing,
+# to prevent overloading of the sensor.
+# Id is the user, that tried to open the door.
+last_change = datetime.utcnow()
 
 
-def show_result(id_key_card, id_door, msg):
-    sense.show_message(str(id_key_card), text_colour = green, back_colour = blue)
+def handle_input(idKey):
+    global last_change
+    # Guard clause, that prevents overloading of the sensor.
+    if (datetime.utcnow() - last_change).seconds >= 1:
+        last_change = datetime.utcnow()
+        # Only allow entry, if the id is a power of 2.
+        if idKey % 2 == 0:
+            accept_entry(idKey)
+        else:
+            deny_entry(idKey)
 
 
-sense.stick.direction_any = anti_spam
+# Function that represents a succesfull access to the lock.
+def accept_entry(idKey):
+    e = colors["empty"]
+    g = colors["green"]
+    pixels = [
+        e, e, e, e, e, e, e, e,
+        e, e, e, e, e, e, e, g,
+        e, e, e, e, e, e, g, e,
+        e, e, e, e, e, g, e, e,
+        g, e, e, e, g, e, e, e,
+        e, g, e, g, e, e, e, e,
+        e, e, g, e, e, e, e, e,
+        e, e, e, e, e, e, e, e
+    ]
+    sense.set_pixels(pixels)
+    # DoBroadcast
+
+
+# Function that represents a failed access to the lock.
+def deny_entry(idKey):
+    e = colors["empty"]
+    r = colors["red"]
+    pixels = [
+        e, e, e, e, e, e, e, e,
+        e, r, e, e, e, e, r, e,
+        e, e, r, e, e, r, e, e,
+        e, e, e, r, r, e, e, e,
+        e, e, e, r, r, e, e, e,
+        e, e, r, e, e, r, e, e,
+        e, r, e, e, e, e, r, e,
+        e, e, e, e, e, e, e, e
+    ]
+    sense.set_pixels(pixels)
+
+
+sensor_id = get_sensor_mac_address()
+sense.stick.direction_any = trigger_open
 
 while True:
     pass
